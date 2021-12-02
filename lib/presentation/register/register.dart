e63102d147fc1a69a66df0caf1bfce999539f478
Hjarambulo <hj.arambulo92@gmail.com>
2021-12-01 22:47:05 -0300

    register form created

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -9,8 +9,78 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: RegisterBar(), body: Container());
+    return Scaffold(
+      appBar: RegisterBar(),
+      body: _createForm(),
+    );
+  }
+
+  _createForm() {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'First Name',
+              )),
+              Divider(),
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Last Name',
+              )),
+              Divider(),
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Email',
+              )),
+              Divider(),
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Confirm Email',
+              )),
+              Divider(),
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Password',
+              )),
+              Divider(),
+              TextFormField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Confirm Password',
+              )),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    AlertDialog(
+                      title: Text('submited'),
+                    );
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
